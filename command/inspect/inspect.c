f9c53235c5db616789e2b5ffb7a6f5e6627f457d
WillWG88 <70704626+WillWG88@users.noreply.github.com>
2021-11-18 22:19:02 +0700

    update

---
+++ command/inspect/inspect.c
@@ -0,0 +1,8 @@
+/* File :inspect.c */
+/* Deskripsi : Command ini akan meminta suatu petak X, lalu melihat apakah ada teleporter di suatu petak X.*/
+/* Isi : Realisasi dari inspect.h*/
+
+
+int main(){
+    
+}
--- command/inspect/inspect.h
+++ command/inspect/inspect.h
@@ -1,2 +1,5 @@
 /* File :inspect.h */
 
+
+
+
