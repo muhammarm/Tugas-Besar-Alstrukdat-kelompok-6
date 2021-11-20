155e4ed39cf39b6218698278621a5b66d1e0c042
rahmatalf_ <rahmatalfajri1@gmail.com>
2021-11-20 18:34:35 +0700

    Perbaikan ADT Map

--- ADT/inspect/arraypeta.h
+++ ADT/inspect/arraypeta.h
@@ -8,19 +8,27 @@
 
 #define IdxMinPetak 1
 #define IdxMaxPetak 100
+#define NoTP -1
 
 
 /*Peta pada permainan ini terdiri dari petak-petak yang digambarkan dengan character “.” (titik, menunjukkan petak kosong) dan “#” (tagar, menunjukkan petak terlarang).*/
 /* Pemain hanya dapat berdiri di petak kosong. Petak pertama dan petak terakhir dijamin berupa petak kosong.Peta dibaca melalui file konfigurasi.*/
 
-typedef char Petak; /*Petak merupakan character*/
+typedef struct {
+        char Petak; /* Jenis petak, bisa petak kosong (.) atau petak terlarang (#) */
+        int TP; /* Indeks tujuan teleporter dari Petak, apabila tidak ada teleporter bernilai NoTP */
+} InfoPetak;
 
 typedef struct {
-        Petak Peta[IdxMaxPetak-IdxMinPetak+1+1]; /* memori tempat penyimpan elemen pada peta(container), peta meurpakan array of character */
+        InfoPetak Peta[IdxMaxPetak-IdxMinPetak+1+1]; /* memori tempat penyimpan elemen pada peta(container), peta merupakan array of character */
+        int Neff; /* Banyaknya elemen array Petak yang dipakai */
 } TabPeta;
 
 void MakeEmptyMap (TabPeta *P);
 /* Menghasilkan tabel peta P kosong dengan kapasitas (IdxMaxPetak-IdxMinPetak+1) */
+
+void ReadMap (TabPeta *P);
+/* Menghasilkan tabel peta P sesuai dengan peta pada file konfigurasi */
 
 void PosisiPemain(TabPeta P, int PosisiPemain, int PanjangPeta);
 /*menampilkan posisi pemain pada peta*/
