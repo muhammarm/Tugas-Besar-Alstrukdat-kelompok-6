0dab97c4123f8b385e1947a7c1da93ab62ab8b7e
WillWG88 <70704626+WillWG88@users.noreply.github.com>
2021-11-21 01:31:28 +0700

    adt player ( kayaknya buat nyimpan informasi pemain), masih kurang fungsinya

---
+++ ADT/player/adtplayer.c
@@ -0,0 +1,9 @@
+/*File : adtplayer.c*/
+/* ADT player untuk menyimpan informasi-informasi terkait player */
+/*implementasi dari adtplayer.h*/
+
+#include "adtplayer.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
