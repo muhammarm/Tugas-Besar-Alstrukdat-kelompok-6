510ff9bc696089dfbdf3dbcbb5d779ac3e7d5ba2
WillWG88 <70704626+WillWG88@users.noreply.github.com>
2021-11-21 15:29:40 +0700

    update adtplayer.h (cek lagi)

--- ADT/player/adtplayer.h
+++ ADT/player/adtplayer.h
@@ -8,14 +8,39 @@
 #include <stdlib.h>
 
 #define Nil NULL
-
-/* Definisi elemen dan koleksi objek */
-typedef int ElType;
 #define IdxMax 4
 #define IdxMin 1
 #define IdxUndef -999
 
-typedef struct listSkillNode *address;
+/* Definisi Type Data LinkedList */
+typedef struct ListSkillNode *address;
+typedef struct ListSkillNode {
+    address NextSkill;
+    char NamaSkill[30];  /* deklarasi string sepanjang 30 karakter */
+} SkillNode;
+typedef struct {
+    address FirstSkill;
+}Skill;
+
+/* Definisi list : */
+/* List kosong : First(L) = Nil */
+/* Setiap elemen dengan address P dapat diacu Info(P), Next(P) */
+/* Elemen terakhir list : jika addressnya Last, maka Next(Last)=Nil */
+
+/*notasi akses*/
+#define FIRSTSKILL(p) (p).FirstSkill
+#define NEXTSKILL(p) (p)->NextSkill
+#define NAMASKILL(p) (p)->NamaSkill
+
+/*player menggunakan implementasi array*/
+typedef int ElType;
+
+typedef struct {
+    int Neff;
+    
+}
+
+
 
 
 
