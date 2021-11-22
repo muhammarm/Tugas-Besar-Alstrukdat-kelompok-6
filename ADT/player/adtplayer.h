39a1024e3847dcab50b73e98775228772917cb14
WillWG88 <70704626+WillWG88@users.noreply.github.com>
2021-11-22 16:02:04 +0700

    update player

--- ADT/player/adtplayer.h
+++ ADT/player/adtplayer.h
@@ -9,7 +9,7 @@
 
 /*Kamus Umum*/
 #define Nil NULL
-#define IdxMax 4
+#define IdxMax 5
 #define IdxMin 1
 #define IdxUndef -999
 
@@ -33,21 +33,21 @@ typedef struct {
 #define NEXTSKILL(P) (P)->NextSkill
 #define NAMASKILL(P) (P)->NamaSkill
 
-/*player menggunakan implementasi array*/
+/*Data player menggunakan implementasi array*/
 typedef int ElType;
 
 typedef struct 
 {
     int Neff;
-    char NamaPemain[IdxMax-IdxMin+1+1][16];
-    ElType Pos[IdxMax - IdxMin + 1+1];
-    boolean IsTeleporter[IdxMax - IdxMin + 1+1];
-    boolean IsImmune[IdxMax - IdxMin + 1+1];
-    boolean IsMirror[IdxMax - IdxMin + 1+1];
-    boolean IsSenterPengecil[IdxMax - IdxMin + 1+1];
-    boolean IsSenterPembesar[IdxMax - IdxMin + 1+1];
-    boolean IsMesinPenukarPos[IdxMax-IdxMin+1+1];
-    boolean IsTeknologiFail[IdxMax-IdxMin+1+1];
+    char NamaPemain[IdxMax-IdxMin+1][16];
+    ElType Pos[IdxMax - IdxMin +1];
+    boolean IsTeleporter[IdxMax - IdxMin +1];
+    boolean IsImmune[IdxMax - IdxMin +1];
+    boolean IsMirror[IdxMax - IdxMin +1];
+    boolean IsSenterPengecil[IdxMax - IdxMin+1];
+    boolean IsSenterPembesar[IdxMax - IdxMin +1];
+    boolean IsMesinPenukarPos[IdxMax-IdxMin+1];
+    boolean IsTeknologiFail[IdxMax-IdxMin+1];
 } Pemain;
 
 /*Selektor(array)*/
@@ -70,9 +70,20 @@ void CreateEmptyPlayerList(Pemain *P);
 boolean IsPemainTeleported(Pemain P, char *NamaPemain);
 /* Menghasilkan True jika pemain dengan parameter NamaPemain terkena portal atau sebaliknya*/
 
-boolean IsPemainImmune(Pemain Pm char *NamaPemain);
-/*Menghasilkan True jika pemain dengan parameter NamaPemain sedang imun atau sebaliknya*/
+boolean IsPemainImmune(Pemain P, char *NamaPemain);
+/*Menghasilkan True jika pemain dengan parameter NamaPemain sedang imun(buff) atau sebaliknya*/
 
+boolean IsPemainSenterPengecil(Pemain P,char *NamaPemain);
+/* Menghasilkan True jika pemain dengan parameter NamaPemain memakai senter pengecil atau sebaliknya*/
+
+boolean IsPemainSenterPembesar(Pemain P, char *NamaPemain);
+/*Menghasilkan True jika pemain dengan parameter NamaPemain memakai senter pembesar atau sebaliknya*/
+
+boolean IsPemainCerminPengganda(Pemain P,char *NamaPemain);
+/*Menghasilkan True jika pemain dengan parameter NamaPemain memakai cermin pengganda atau sebaliknya*/
+
+
+ 
 
 
 
