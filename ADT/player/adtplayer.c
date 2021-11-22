1106eda8c527a231458e72d6b7b33b2fdba91de9
Michelle Angelica <angelicalidwina@gmail.com>
2021-11-22 22:04:37 +0700

    Update adtplayer.c

--- ADT/player/adtplayer.c
+++ ADT/player/adtplayer.c
@@ -7,3 +7,103 @@
 #include <stdio.h>
 #include <string.h>
 
+/* Membuat List Pemain yang kosong */
+void CreateEmptyPlayerList(Pemain *P)
+{
+    (*P).Neff = 0;
+
+}
+
+/* Membuat List Pemain sebanyak n pemain */
+void addPlayer(Pemain *P, int n)
+{
+    for (int x = 1; x<=n; x++){
+        printf("Masukan nama pemain %d: ", i);
+        scanf("%s", &(NamaPemain(*P)[i]));
+        int y = 1;
+        while (y <= n){
+            if(strcmp(NamaPemain(*P)[x], NamaPemain(*P)[y]) == 0 && y != x){
+                printf("Nama %s sudah digunakan!\n", NamaPemain(*P)[x]);
+                printf("Masukkan nama lain untuk pemain %d: ", i);
+                scanf("%s", &(NamaPemain(*P)[x]))
+            }
+            else{
+                y++;
+            }
+        }
+        Pos(*P)[x] = 1;
+        IsTeleporter(*P)[x] = false;
+        IsImmune(*P)[x] = false;
+        IsSenterPembesar(*P)[x] = false;
+        IsSenterPengecil(*P)[x] = false;
+        IsMirror(*P)[x] = false;
+        IsMesinPenukarPos(*P)[x] = false;
+        IsTeknologiFail(*P)[x] = false;
+    }
+    Neff(*P) = n;
+}
+/* Mendapatkan index pemain */
+int getPemainIdx(Pemain *P, char *Nama)
+{
+    int x = IdxMin;
+    int idxP = IdxUndef;
+    while ( x <= IdxMax){
+        if(strcmp(Nama, P.NamaPemain[x]) != 0){
+            x++;
+        }
+        else {
+            idxP = x;
+            break;
+        }
+    }
+    return idxP;
+}
+
+boolean IsPemainTeleported(Pemain P, char *NamaPemain);
+/* Menghasilkan True jika pemain dengan parameter NamaPemain terkena portal atau sebaliknya */
+{
+    int idx = getPemainIdx(P, NamaPemain);
+    if (idx != IdxUndef){
+        return (IsTeleporter(P)[idx]);
+    }
+}
+
+boolean IsPemainImmune(Pemain P, char *NamaPemain);
+/*Menghasilkan True jika pemain dengan parameter NamaPemain sedang imun(buff) atau sebaliknya*/
+{
+    int idx = getPemainIdx(P, NamaPemain);
+    if (idx != IdxUndef){
+        return (IsImmune(P)[idx]);
+    }
+}
+
+boolean IsPemainSenterPengecil(Pemain P,char *NamaPemain);
+/* Menghasilkan True jika pemain dengan parameter NamaPemain memakai senter pengecil atau sebaliknya*/
+{
+    int idx = getPemainIdx(P, char *NamaPemain);
+    if (idx != IdxUndef){
+        return(IsSenterPengecil(P)[idx]);
+    }
+}
+
+boolean IsPemainSenterPembesar(Pemain P, char *NamaPemain);
+/*Menghasilkan True jika pemain dengan parameter NamaPemain memakai senter pembesar atau sebaliknya*/
+{
+    int idx = getPemainIdx(P, char *NamaPemain);
+    if (idx != IdxUndef){
+        return(IsSenterPembesar(P)[idx]);
+    }
+}
+
+boolean IsPemainCerminPengganda(Pemain P,char *NamaPemain);
+/*Menghasilkan True jika pemain dengan parameter NamaPemain memakai cermin pengganda atau sebaliknya*/
+{
+    int idx = getPemainIdx(P, char *NamaPemain);
+    if (idx != IdxUndef){
+        return(IsMirror(P)[idx]);
+    }
+}
+
+
+
+
