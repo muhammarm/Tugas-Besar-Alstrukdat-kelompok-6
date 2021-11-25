ee8fcbab7d3c331c48ddd13d2016033b973aeef9
WillWG88 <70704626+WillWG88@users.noreply.github.com>
2021-11-25 16:36:09 +0700

    Create main.c

---
+++ ADT/main/main.c
@@ -0,0 +1,23 @@
+/* File : main.c*/
+/* berisi program utama untuk menjalankan permainan*/
+
+#include "mesin_kar.h"
+#include "mesin_kata.h"
+#include "arraypeta.h"
+#include "roll.h"
+#include "adtplayer.h" /*untuk adt player*/
+#include "listdp.h" /*untuk adt skill*/
+#include "stack.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    int jumlahpemain;
+    Pemain P; /*dari adt player*/
+    TabPeta Peta;/*dari arraypeta*/
+    printf("Masukkan jumlah pemain game : ");
+    scanf("%d",&jumlahpemain);
+    printf("\n") ;
+    
+}
