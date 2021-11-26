bd4204f9a6cffa9e30b85ee5658a56559719d04f
WillWG88 <70704626+WillWG88@users.noreply.github.com>
2021-11-26 21:51:09 +0700

    Update command.c

--- ADT/command buat main/command.c
+++ ADT/command buat main/command.c
@@ -8,19 +8,46 @@
 void command(TabPeta *Peta,Pemain *P,Stack *Stack,int PlayerTurn,boolean *EndGame, boolean *EndRonde, int *SumRonde)
 {
     /*Kamus*/
-    int x;
-    int HasilCommand;
-    boolean endturn=false;
-
+    char CommandInput[100];
     /*Algoritma*/
-    
-    /*while loop*/
-     while (!endturn)
-     {
-         HasilCommand=0;
-         printf("Masukkan Command : ");
-         
-     }
+    printf("Masukkan Command : ");
+    /*membaca command dari user*/
+    scanf("%s", &CommandInput); 
+    printf("\n")
+
+
+    switch(CommandInput){
+        case "SKILL": 
+        /* Berisi command skill*/
+        
+
+        case "BUFF" :
+        /*Berisi command Buff*/
+
+
+        case "MAP" : 
+        /*Berisi command Map*/
+
+
+        case "INSPECT" :
+        /*Berisi command Buff*/
+
+
+        case "ROLL" :
+        /*Berisi command Roll*/
+
+
+        case "UNDO" :
+        /*Berisi command Undo */
+
+        case "ENDTURN" :
+        /*Berisi command Endturn */
+
+        case "SAVE" :
+
+    }
+
+
     
 
 
