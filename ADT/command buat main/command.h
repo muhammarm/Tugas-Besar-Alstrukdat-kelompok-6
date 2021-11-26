afbeab443886293bb9595f4386aea698d662beb1
muhammadammar497 <abah.amay0331@gmail.com>
2021-11-26 21:24:23 +0700

    update command.h

--- ADT/command buat main/command.h
+++ ADT/command buat main/command.h
@@ -12,18 +12,6 @@
 #include "mesin_kar.h"
 #include "mesin_kata.h"
 
-/* deklarasi dan inisialisasi string*/
-char skill[] = "SKILL";
-char buff[] = "BUFF";     
-char map[] = "MAP";                         
-char inspect[] ="INSPECT";
-char roll[] = "ROLL";
-char save[] = "SAVE";
-char endturn[] ="ENDTURN";
-char undo[] = "UNDO";
-
-/*deklarasi Tab Command */
-char TabCommand[10];
-
 /*prosedur command*/
 void command(TabPeta *Peta,Pemain *P,Stack *Stack,int PlayerTurn,boolean *EndGame, boolean *EndRonde, int *SumRonde); /* SumRonde==Jumlah Ronde */
+#endif
