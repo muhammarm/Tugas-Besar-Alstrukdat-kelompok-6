c6d9598b79e96da2a5ff5fb02dc2bbcd35f41f13
rahmatalf <rahmatalfajri1@gmail.com>
2021-11-27 16:46:06 +0700

    Update driverpeta

--- ADT/Map/driverpeta.c
+++ ADT/Map/driverpeta.c
@@ -4,19 +4,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include "../Boolean/boolean.h"
 #include "arraypeta.h"
+#include "arraypeta.c"
 
 TabPeta Map;
-char filename[100];
 int posisi;
 
 int main(){
-    char filepath[] = "../../config/";
+    char configpath[] = "..\\..\\config\\";
+    char filename[100];
+    char filepath[255];
+
+    strcpy(filepath, configpath);
+    printf("Masukkan nama file konfigurasi : ");
     scanf("%s", &filename);
     strncat(filepath, filename, 100);
-
+    while (!CheckIfFileExists(filepath)){
+        printf("File %s tidak ada, mohon ulangi.\n", filename);
+        strcpy(filepath, configpath);
+        printf("Masukkan nama file konfigurasi : ");
+        scanf("%s", &filename);
+        strncat(filepath, filename, 100);
+    }
     ReadMap(&Map, filepath);
+
     for (int i = 1;i <= Map.Neff;i++){
         printf("Petak ke-%d\n", i);
         printf("Info Petak  : %c\n", GetPetakInfo(Map, i));
