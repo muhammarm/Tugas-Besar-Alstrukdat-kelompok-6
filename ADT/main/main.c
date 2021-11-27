9197009fa16af270a8036a2ffaff020e4070c209
WillWG88 <70704626+WillWG88@users.noreply.github.com>
2021-11-27 16:54:12 +0700

    program mainnya udh selesai tolong cek lagi

--- ADT/main/main.c
+++ ADT/main/main.c
@@ -1,5 +1,5 @@
 /* File : main.c*/
-/* berisi program utama untuk menjalankan permainan*/
+/* berisi program utama untuk menjalankan permainan ular tangga*/
 
 #include "mesin_kar.h"
 #include "mesin_kata.h"
@@ -14,12 +14,9 @@
 
 int main(){
     /*Kamus*/
-    int JumlahPemain, maxroll;
+    int JumlahPemain;
     Pemain P; /*dari adt player*/
-    TabPeta Peta;/*dari arraypeta*/
-    Stack S;
-    char filepath[] = "../../config/";
-    char filename[100];
+    TabPeta PetaGame;/*dari arraypeta*/
 
 
     /*START GAME*/
@@ -40,20 +37,86 @@ int main(){
     }
     printf("\n");
     
-    /*Membuat list player kosong*/
+    /*Membuat list pemain kosong*/
     CreateEmptyPemainList(&P);
     /*membuat list pemain sebanyak "JumlahPemain"*/
     AddPemain(&P,JumlahPemain);
     printf("\n");
 
     /*Program Membaca Map dari File konfigurasi*/
+    int maxroll;
+    char configpath[] =  "..\\..\\config\\";
+    char filename[100];
+    char filepath[255];
+
+    strcpy(filepath, configpath);
+    printf("Masukkan nama file konfigurasi : ");
     scanf("%s", &filename);
     strncat(filepath, filename, 100);
-    ReadMap (&P,filepath);
+    while (!CheckIfFileExists(filepath))
+    {
+        printf("File %s tidak ada, mohon ulangi.\n", filename);
+        strcpy(filepath, configpath);
+        printf("Masukkan nama file konfigurasi : ");
+        scanf("%s", &filename);
+        strncat(filepath, filename, 100);
+    }
+    ReadMap(&PetaGame, filepath);
     maxroll = ReadMaxroll(filepath);
 
     /*Membuat Stack kosong*/
+    Stack S;
     CreateEmpty(&S);
     
 
+    /*Game akan terus looping hingga ada pemain yang menang*/
+    boolean GameBerakhir=false;
+    boolean EndRonde=false;
+    int JumlahRonde = 1;
+
+    Push(&S,&P);
+    while (!GameBerakhir)
+    {
+        int x =1;
+        EndRonde=false;
+        printf("-- Ronde ke-%d --\n\n", JumlahRonde);
+
+        /*Setiap pemain mendapatkan turn pada sebuah ronde*/
+        while (!EndRonde && x <= JumlahPemain)
+        {
+            printf("-- Giliran %s --\n\n", (P).NamaPemain[x]) ;
+            draw(&P,x,0);
+
+            /*Menuliskan Peta di awal giliran*/
+            for(int TulisPemain=1;TulisPemain<=(P).Neff;TulisPemain++)
+            {/*loop untuk menuliskan nama pemain*/
+                printf("%s\t", (P).NamaPemain[TulisPemain]) ;
+                printf(" : ");
+                for(int TulisPetak=1 ; TulisPetak<=PetaGame.Neff;TulisPetak++)
+                {/*loop untuk menampilkan state peta dengan posisi pemain digambarkan dengan "*" */
+                    if((P).Pos[TulisPemain]==TulisPetak)
+                    {
+                        printf("*");
+                    }
+                    else
+                    {
+                        printf("%c", (PetaGame).Peta[TulisPetak].Petak);
+                    }
+                }
+                printf(" %d", P.Pos[TulisPemain]) ; /*->Pada akhir setiap baris ditampilkan petak pemain tersebut*/
+            }
+            printf("\n");
+            
+        /*Prosedur command akan meminta user untuk memasukkan command yang tersedia lalu akan menjalankan fitur command sesuai input user */
+            command(&PetaGame, &P, &S, x, &GameBerakhir, &EndRonde, &JumlahRonde) ;
+            x++;
+        }
+        for (x=1;x<=P.Neff;x++)
+        {
+            P.IsDoneRoll[x]=false;
+        }
+        JumlahRonde++;
+        printf("\n");
+        Push(&S,&P);
+    }
 }
